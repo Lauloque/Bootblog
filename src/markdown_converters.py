8b363491f36b3893d009718dd0c7de979481e621
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-06-25 21:16:41 +0200

    add MD links and images extractors

--- src/markdown_converters.py
+++ src/markdown_converters.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextNode, TextType
 
 
@@ -19,3 +21,11 @@ def split_nodes_delimiter(
             new_nodes.append(TextNode(part, TextType.TEXT if i % 2 == 0 else text_type))
 
     return new_nodes
+
+
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_markdown_converter.py
+++ src/test_markdown_converter.py
@@ -1,6 +1,10 @@
 import unittest
 
-from markdown_converters import split_nodes_delimiter
+from markdown_converters import (
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_delimiter,
+)
 from textnode import TextNode, TextType
 
 
@@ -81,6 +85,47 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         ]
 
         self.assertEqual(result, expected)
+
+
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_extract_link_single(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev)"
+
+        self.assertEqual(
+            extract_markdown_links(text), [("to boot dev", "https://www.boot.dev")]
+        )
+
+    def test_extract_link_multiple(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+
+        self.assertEqual(
+            extract_markdown_links(text),
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+        )
+
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_extract_link_single(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)"
+
+        self.assertEqual(
+            extract_markdown_images(text),
+            [("rick roll", "https://i.imgur.com/aKaOqIh.gif")],
+        )
+
+    def test_extract_link_multiple(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+
+        self.assertEqual(
+            extract_markdown_images(text),
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+        )
 
 
 if __name__ == "__main__":
