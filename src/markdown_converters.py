300ce963eeea6f9cd3eeeb805325e342d2da8882
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-06-28 21:56:17 +0200

    moving blocknode code into markdown_converters.py

--- src/markdown_converters.py
+++ src/markdown_converters.py
@@ -1,6 +1,38 @@
 import re
+from enum import Enum
 
+from src.htmlnode import HTMLNode
 from textnode import TextNode, TextType
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(text: str) -> BlockType:
+    if re.search(r"^(#{1,6}\s)", text):
+        return BlockType.HEADING
+
+    if re.search(r"^```\n[\s\S]*?\n```$", text):
+        return BlockType.CODE
+
+    lines = text.split("\n")
+
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+
+    for i, line in enumerate(lines, start=1):
+        if not line.startswith(f"{i}. "):
+            return BlockType.PARAGRAPH
+    return BlockType.ORDERED_LIST
 
 
 def split_nodes_delimiter(
