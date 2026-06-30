991f78006021abfa0a3e4d571de4b642eead5e00
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-06-30 16:18:09 +0200

    add copy_files_in_dir()

--- src/main.py
+++ src/main.py
@@ -1,14 +1,33 @@
-from textnode import TextNode, TextType
+import shutil
+from os import getcwd, listdir, mkdir, path
 
-print("hello world")
+
+def copy_files_in_dir(basePath: str, outputPath: str) -> None:
+    # Cleanup outputPath
+    if path.exists(outputPath):
+        shutil.rmtree(outputPath)
+    mkdir(outputPath)
+
+    for item in listdir(basePath):
+        itemPath = path.join(basePath, item)
+
+        if path.isdir(itemPath):
+            copy_files_in_dir(itemPath, path.join(outputPath, item))
+        else:
+            print(f"copying '{itemPath}' to '{outputPath}'")
+            shutil.copy(itemPath, outputPath)
+
+
+def generate_site() -> None:
+    cwd = getcwd()
+    static_dir = path.join(cwd, "static")
+    public_dir = path.join(cwd, "public")
+
+    copy_files_in_dir(static_dir, public_dir)
 
 
 def main():
-    my_node = TextNode(
-        "This is some anchor text", TextType.LINK, "https://www.boot.dev"
-    )
-
-    print(my_node)
+    generate_site()
 
 
 if __name__ == "__main__":
